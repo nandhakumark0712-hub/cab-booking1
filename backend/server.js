cae31fd369bef264c9155343acdf1947d66a29fa
Nandhakumar <nandhakumar.k0712@gmail.com>
2026-02-26 11:44:39 +0530

    update frontend cors url

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,10 @@ const app = express();
 const server = http.createServer(app);
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: "https://cab-booking1-iota.vercel.app"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
